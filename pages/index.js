4936e136b17d1c754ef87336a5ab1e636b5c4ce9
markovad98 <markovalexeydmit@gmail.com>
2020-02-11 07:46:05 +0400

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import { Button, Alert } from "reactstrap";
+import Layout from "../components/Layout";
+export default () => {
+    return (
+        <Layout>
+            <div>
+                <Alert color="primary">
+                    Hello Project is strapi-next with Bootstrap
+                </Alert>
+                &nbsp; <Button color="primary">Hello from nextjs</Button>
+            </div>
+        </Layout>
+    );
+};
---
+++ pages/signin.js
@@ -0,0 +1,11 @@
+
+/* pages/signin.js */
+import Layout from "../components/Layout";
+
+export default () => {
+    return (
+        <Layout>
+            <h1>Sign In</h1>
+        </Layout>
+    );
+};
